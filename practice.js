f9e7d1382227c5ac310829079338ec0ffeaa6061
Ben Anderson <ben@Bens-MacBook-Pro.local>
2019-09-18 15:26:52 -0500

    loops are very hard

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first (arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last (arr){
+  return arr[2]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for (i=0; i < family.length; i++){
+    alert(family[i])
+  }
+  console.log(family)
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +72,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for (i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+  console.log(letters)
+}
 
 
 ////////// PROBLEM 5 //////////
